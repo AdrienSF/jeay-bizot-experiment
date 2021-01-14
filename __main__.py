7237ebe76b3fa892d152c6d4cd69fae706280ff9
Adrien Schurger-Foy <adriensf@gmail.com>
2021-01-14 10:47:15 -0800

    add accelerometer compatibility

--- __main__.py
+++ __main__.py
@@ -2,6 +2,14 @@ from psychopy import visual, core, event
 from high_striker import HighStriker
 from traffic_light import TrafficLight
 from session import Session
+from pylsl import StreamInlet, resolve_stream
+
+print("looking for stream...")
+streams = resolve_stream('source_id', 'myuid323457') 
+
+inlet = StreamInlet(streams[0])
+print('found')
+
 
 sess_id = input('enter session ID: ')
 win = visual.Window(monitor='testMonitor', fullscr=True)
@@ -12,7 +20,7 @@ light = TrafficLight(win)
 
 striker = HighStriker(win, top_coords=(0, windowRect[1]/2-100), bottom_coords=(0, -1*(windowRect[1]/2-100)))
 
-session = Session(win, striker, light, session_id=sess_id)# not supposed to be mouse but instead an input stream from sensor
+session = Session(win, striker, light, session_id=sess_id, sensor_input_stream=inlet)
 
 session.run()
 
--- session.py
+++ session.py
@@ -1,9 +1,10 @@
 from high_striker import HighStriker
 from traffic_light import TrafficLight
-from speedometer import Speedometer
-from psychopy import visual, core, event
+# from speedometer import Speedometer [DEPRECATED]
+from psychopy import visual, core, event, parallel
 import math
 import csv
+import statistics as stat
 
 
 
@@ -13,15 +14,20 @@ class Session(object):
         self.striker = striker
         self.light = light
         self.session_id = session_id
-        self.block_duration = 5*60
+        self.stream_inlet = sensor_input_stream
+
+        self.block_duration = 30
+        self.current_block = 0
+        self.current_trial = 0
+        # self.total_blocks = 2
         # self.input_stream = sensor_input_stream
         # self.mouse = sensor_input_stream # not supposed to be mouse but instead an input stream from sensor
 
-        self.speed_thresh = 10 # a hundred what? [NOTE]: need to check this
+        self.acc_thresh = 10 # a hundred what? [NOTE]: need to check this
 
         self.clock = None
+        self.post_pulse_time = None
         self.history = {}
-        self.current_trial = 0
 
         self.instructions = {True: 'Launch the slider to wherever you want, and whenever, but try to space out the launches in time.',
                             False: 'Launch the slider as precisely as possible into the randomly appearing red target. You have a limited number of tries so make every shot count.'
@@ -31,7 +37,7 @@ class Session(object):
     
     def run_trial(self, is_type_A: bool):
         self.current_trial += 1
-        self.history[self.current_trial] = {}
+        self.history[self.current_block][self.current_trial] = {}
 
 
         self.light.is_green = False
@@ -44,13 +50,13 @@ class Session(object):
 
         self.light.is_green = True
         y_pos = self.striker.randomize_target()
-        self.history[self.current_trial]['target_y_pos'] = y_pos
+        self.history[self.current_block][self.current_trial]['target_y_pos'] = y_pos
 
         self.light.draw()
         self.striker.draw(no_target=is_type_A)
         self.window.flip()
         # record time when light turned on
-        self.history[self.current_trial]['ON_disp_time'] = self.clock.getTime()
+        self.history[self.current_block][self.current_trial]['ON_disp_time'] = self.clock.getTime()
 
         # start listening to self.input_stream
         # do recording stuff
@@ -70,48 +76,58 @@ class Session(object):
         
 
     def get_dist(self, dt=.020):
-        mouse = event.Mouse(win=self.window)
-        clock = core.Clock()
-        speedometer = Speedometer(mouse, clock)
-        interval = []
-        speed = 0
-        while speed < self.speed_thresh:
-            speed = speedometer.get_speed(dt)
+        # mouse = event.Mouse(win=self.window)
+        # clock = core.Clock()
+        # speedometer = Speedometer(mouse, clock)
+        # interval = []
+        acc = 0
+        while acc < self.acc_thresh:
+            acc, timestamp = self.stream_inlet.pull_sample()
             # print(speed)
         # record start of movement
-        self.history[self.current_trial]['movement_onset'] = self.clock.getTime()
+        self.history[self.current_block][self.current_trial]['movement_onset'] = self.clock.getTime()
         
         # reset clock and speedometer
-        clock = core.Clock()
-        speedometer = Speedometer(mouse, clock)
-        speed = math.inf
-        while speed > self.speed_thresh:
-            speed = speedometer.get_speed(dt)
+        # clock = core.Clock()
+        # speedometer = Speedometer(mouse, clock)
+        acc = math.inf
+        acc_hist = []
+        while acc > self.acc_thresh:
+            acc, timestamp = self.stream_inlet.pull_sample()
+            acc_hist.append(acc)
         # record end of movement        
-        self.history[self.current_trial]['movement_end'] = self.clock.getTime()
+        self.history[self.current_block][self.current_trial]['movement_end'] = self.clock.getTime()
 
-        speed = speedometer.get_mean_speed()
-        self.history[self.current_trial]['speed'] = speed
-        return speed * 20
+        mean_acc = stat.mean(acc_hist)
+        self.history[self.current_block][self.current_trial]['acceleration'] = mean_acc
+
+        return mean_acc/1000000
 
     def run_block(self, is_type_A):
+        self.current_block += 1
+        self.current_trial = 0
+        self.history[self.current_block] = {}
+
         self.display_instructions(is_type_A)
         block_start = self.clock.getTime()
         while self.clock.getTime() < self.block_duration + block_start:
             self.run_trial(is_type_A)
 
     def run(self):
+        port = parallel.ParallelPort(address='0x0378')
         self.clock = core.Clock()
         # insert pulse to EEG
+        port.setData(1)
+        self.post_pulse_time = self.clock.getTime()
         for trial_type in [True, False]:
             self.run_block(trial_type)
 
 
     def save_to_csv(self):
-        top = ['session ID: ' + str(self.session_id)]
-        header = list(['trial_num'] + list(list(self.history.values())[0].keys()))
+        top = ['session_ID: ' + str(self.session_id), 'post_pulse_time: ' + str(self.post_pulse_time)]
+        header = list(['block_num', 'trial_num'] + list(list(list(self.history.values())[0].values())[0].keys()))
 
-        contents = [ [trial_num] + list(self.history[trial_num].values()) for trial_num in self.history ]
+        contents = [ [block_num, trial_num] + list(self.history[block_num][trial_num].values()) for block_num in self.history for trial_num in self.history[block_num] ]
 
         with open(str(self.session_id) + '.csv', 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',',
