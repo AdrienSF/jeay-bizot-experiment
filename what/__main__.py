2b4c851b23a409a706afd31383182c6cf075e6c6
Lucas Jeay-Bizot <lucas@Lucass-MacBook-Pro.local>
2021-05-27 18:02:02 -0700

    added scale

--- what/__main__.py
+++ what/__main__.py
@@ -2,10 +2,11 @@ from psychopy import visual, core, event
 from session import WhatSession
 
 sess_id = input('enter session ID: ')
+comp_device = input('are you on the stimulus comp (s) or at home (h): ')
 win = visual.Window(monitor='testMonitor', fullscr=True)
 windowRect = win.size
 
-session = WhatSession(win, sess_id)
+session = WhatSession(win, sess_id, comp_device)
 
 try:
     session.run()
